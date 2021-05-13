9b0de3a4ae725562a048ada5672671f8a8012bf0
Milan Karman <milankarman@gmail.com>
2021-05-13 02:09:17 +0200

    More layout work and cleaning up code

---
+++ auto-loadless/FFMPEG.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Diagnostics;
+using System.Windows;
+
+namespace auto_loadless
+{
+    public static class FFMPEG
+    {
+        public static void ConvertToImageSequence(string inputPath, string outputPath)
+        {
+            string ffmpegPath = Path.Join("C:", "Program Files", "ffmpeg", "bin", "ffmpeg.exe");
+
+            Process process = new Process();
+            process.StartInfo.RedirectStandardOutput = false;
+            process.StartInfo.RedirectStandardError = false;
+
+            process.StartInfo.FileName = ffmpegPath;
+            process.StartInfo.Arguments = $"-i {inputPath} -r 1/1 {outputPath}/%0d.bmp";
+            // process.StartInfo.Arguments = $"-i {inputPath} -s 640x480 -vf fps=1 {outputPath}/%d.jpg";
+
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = false;
+            process.Start();
+        }
+    }
+}
---
+++ auto-loadless/ImageProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows;
+using Microsoft.Win32;
+using Shipwreck.Phash;
+using Shipwreck.Phash.Bitmaps;
+
+namespace auto_loadless
+{
+    public static class ImageProcessor
+    {
+        public static float ComparePhash(Bitmap image1, Bitmap image2)
+        {
+            Digest hash1 = ImagePhash.ComputeDigest(image1.ToLuminanceImage());
+            Digest hash2 = ImagePhash.ComputeDigest(image2.ToLuminanceImage());
+
+            float score = ImagePhash.GetCrossCorrelation(hash1, hash2);
+
+            return score;
+        }
+    }
+}
