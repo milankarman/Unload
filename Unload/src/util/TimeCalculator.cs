4e440ba28fd4d4c145b9a3566316f0ab1e46dab4
Milan Karman <milankarman@gmail.com>
2022-02-12 00:14:52 +0100

    Moved load calculation into utility class

---
+++ Unload/src/util/TimeCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace unload
+{
+    public static class TimeCalculator
+    {
+        // Verifies user input is correct and returns the total (with loads) time formatted as a string
+        public static string GetTotalTimeString(double framesPerSecond, int totalFrames)
+        {
+            double totalSecondsDouble = totalFrames / framesPerSecond;
+            TimeSpan timeWithoutLoads = TimeSpan.FromSeconds(Math.Round(totalSecondsDouble, 3));
+
+            return $"{timeWithoutLoads:hh\\:mm\\:ss\\.fff}";
+        }
+
+        // Verifies user input is correct and returns the loadless time formatted as a string
+        public static string GetLoadlessTimeString(double framesPerSecond, int totalFrames, int loadFrames)
+        {
+            int loadlessFrames = totalFrames - loadFrames;
+            double loadlessSecondsDouble = loadlessFrames / framesPerSecond;
+            TimeSpan timeWithoutLoads = TimeSpan.FromSeconds(Math.Round(loadlessSecondsDouble, 3));
+
+            return $"{timeWithoutLoads:hh\\:mm\\:ss\\.fff}";
+        }
+
+        // Verifies user input is correct and returns the loadless time formatted as a string
+        public static string GetTimeSpentLoadingString(double framesPerSecond, int totalFrames, int loadFrames)
+        {
+            double loadlessSecondsDouble = loadFrames / framesPerSecond;
+            TimeSpan timeSpentLoading = TimeSpan.FromSeconds(Math.Round(loadlessSecondsDouble, 3));
+
+            return $"{timeSpentLoading:hh\\:mm\\:ss\\.fff}";
+        }
+    }
+}
---
+++ Unload/src/windows/StartWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using Shipwreck.Phash;
+using unload.Properties;
+using Xabe.FFmpeg;
+
+namespace unload
+{
+    public partial class StartWindow : Window
+    {
+        public string? workingDirectory = null;
+        private string? targetDirectory = null;
+        private const string FRAMES_SUFFIX = "_frames";
+
+        public StartWindow()
+        {
+            InitializeComponent();
+
+            // Confirm FFmpeg is available
+            try
+            {
+                VideoProcessor.SetFFMpegPath();
+            }
+            catch
+            {
+                string message = "Failed to initialize FFMpeg. Make sure ffmpeg.exe and ffprobe.exe are located in the ffmpeg folder of this application.";
+                MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Shutdown();
+            }
+        }
+
+        public void StartMainWindow(string fileName)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.LoadFolder(fileName, targetDirectory);
+            mainWindow.Show();
+            Close();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                // Remove symbols from path and append _frames
+                string? fileDirectory = Path.GetDirectoryName(dialog.FileName);
+
+                targetDirectory = Path.Join(
+                    workingDirectory ?? fileDirectory,
+                    RemoveSymbols(dialog.SafeFileName) + FRAMES_SUFFIX);
+
+                if (!Directory.Exists(targetDirectory))
+                {
+                    MessageBox.Show(
+                        $"No {FRAMES_SUFFIX} folder accompanying this video found. Convert the video first.",
+                        "Error",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    return;
+                }
+
+                StartMainWindow(dialog.FileName);
+            }
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                // Create _frames folder to store the image sequence, ommiting illegal symbols
+                string? fileDirectory = Path.GetDirectoryName(dialog.FileName);
+
+                targetDirectory = Path.Join(
+                    workingDirectory ?? fileDirectory,
+                    RemoveSymbols(dialog.SafeFileName) + FRAMES_SUFFIX);
+
+                if (!Directory.Exists(targetDirectory))
+                {
+                    Directory.CreateDirectory(targetDirectory);
+                }
+
+                IsEnabled = false;
+                ConvertWindow convertWindow = new ConvertWindow(this, dialog.FileName, targetDirectory);
+                convertWindow.GetVideoInfoAndShow();
+            }
+        }
+
+        private void btnStartSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Removes symbols that conflict with FFmpeg arguments
+        private static string RemoveSymbols(string path)
+        {
+            return Regex.Replace(path, @"[^0-9a-zA-Z\/\\:]+", "");
+        }
+    }
+}
